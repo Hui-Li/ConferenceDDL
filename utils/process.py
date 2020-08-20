7f1cb1382a9f52494557994f5b6671281aa9aa30
Hui Li <7204277+Hui-Li@users.noreply.github.com>
2020-08-20 17:45:26 +0800

    update ddl

--- utils/process.py
+++ utils/process.py
@@ -61,39 +61,6 @@ right_now = datetime.datetime.utcnow().replace(
     microsecond=0).strftime(dateformat)
 
 
-# Helper function for yes no questions
-def query_yes_no(question, default="no"):
-    """Ask a yes/no question via input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is True for "yes" or False for "no".
-    """
-    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
-
-
 # Sort:
 
 with open("../_data/conferences.yml", 'r') as stream:
